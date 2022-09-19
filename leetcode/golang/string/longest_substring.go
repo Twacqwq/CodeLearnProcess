694514342e57d4bde62b2b1c0089ebbb2e515cf7
twacqwq <xiaot.qaq@gmail.com>
2022-09-19 14:42:26 +0800

    add

---
+++ leetcode/golang/string/longest_substring.go
@@ -0,0 +1,29 @@
+package main
+
+import "math"
+
+// https://leetcode.cn/problems/longest-substring-without-repeating-characters
+
+func lengthOfLongestSubstring(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+	res := [127]int{}
+	result, left, right := 0, 0, -1
+	for left < len(s) {
+		if right+1 < len(s) && res[s[right+1]] == 0 {
+			res[s[right+1]]++
+			right++
+		} else {
+			res[s[left]]--
+			left++
+		}
+		result = int(math.Max(float64(result), float64(right-left+1)))
+	}
+	return result
+}
+
+func main() {
+	s := "abcabcbb"
+	println(lengthOfLongestSubstring(s))
+}
