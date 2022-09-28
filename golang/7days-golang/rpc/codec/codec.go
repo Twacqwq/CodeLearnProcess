f2a9de3cecf161f0eeaf06b2099b0b5984a3452b
twacqwq <xiaot.qaq@gmail.com>
2022-09-29 01:11:21 +0800

    add geerpc

---
+++ golang/7days-golang/rpc/codec/codec.go
@@ -0,0 +1,32 @@
+package codec
+
+import "io"
+
+type Header struct {
+	Seq           uint64 // 请求标识
+	ServiceMethod string // format: "Airth.Sum"
+	Error         error
+}
+
+// 消息体编解码器
+type Codec interface {
+	io.Closer
+	ReadHeader(*Header) error
+	ReadBody(interface{}) error
+	Write(*Header, interface{}) error
+}
+
+type NewCodecFunc func(io.ReadWriteCloser) Codec
+
+type Type string
+
+const (
+	JsonType = "application/json"
+	GobType  = "application/gob"
+)
+
+var NewCodecFuncMap map[Type]NewCodecFunc
+
+func init() {
+	NewCodecFuncMap = make(map[Type]NewCodecFunc)
+}
