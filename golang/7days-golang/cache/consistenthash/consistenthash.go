33038ad0ac78f855d736facfea95f4a48ef2fcbb
twacqwq <xiaot.qaq@gmail.com>
2022-10-25 16:16:41 +0800

    consistenthash

--- golang/7days-golang/cache/consistenthash/consistenthash.go
+++ golang/7days-golang/cache/consistenthash/consistenthash.go
@@ -1,3 +1,56 @@
 package consistenthash
 
+import (
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
+
 type Hash func(data []byte) uint32
+
+// 映射包含所有散列键
+type Map struct {
+	hash     Hash
+	replicas int            //虚拟节点倍数
+	keys     []int          // hash环
+	hashMap  map[int]string // 虚拟节点与真实节点的映射
+}
+
+func New(replicas int, fn Hash) *Map {
+	m := &Map{
+		hash:     fn,
+		replicas: replicas,
+		hashMap:  make(map[int]string),
+	}
+	if m.hash == nil {
+		m.hash = crc32.ChecksumIEEE
+	}
+	return m
+}
+
+// 将一些键添加到哈希中
+func (m *Map) Add(keys ...string) {
+	for _, key := range keys {
+		for i := 0; i < m.replicas; i++ {
+			hash := int(m.hash([]byte(strconv.Itoa(i) + key)))
+			m.keys = append(m.keys, hash)
+			m.hashMap[hash] = key
+		}
+	}
+	sort.Ints(m.keys)
+}
+
+func (m *Map) Get(key string) string {
+	if len(m.keys) == 0 {
+		return ""
+	}
+
+	hash := int(m.hash([]byte(key)))
+	// 找到合适的副本(包括虚拟节点)
+	idx := sort.Search(len(m.keys), func(i int) bool {
+		return m.keys[i] >= hash
+	})
+
+	// m.keys[idx%len(m.keys)] 得到真实的节点 因为 m.keys 是一个环状结构，所以用取余数的方式来处理这种情况。
+	return m.hashMap[m.keys[idx%len(m.keys)]]
+}
